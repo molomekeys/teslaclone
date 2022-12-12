c7ab02ece6dd13adf7645aa0364d62d490e8fcd9
Ouzar Merouane <ouzar.merouane@gmail.com>
2022-12-12 15:46:39 -0500

    added the features

--- components/BenchMark.js
+++ components/BenchMark.js
@@ -1,15 +1,18 @@
-
+import { motion } from 'framer-motion'
 export default function BenchComponent(props) {
     return (
-        <div className="grid grid-cols-1 lg:flex text-center place-items-center ">
+        <motion.div
 
-            <ol className="flex  w-full gap-8  self-center lg:grid-cols-4 ">
+            className="grid grid-cols-1 lg:flex text-center place-items-stretch gap-20">
+
+            <motion.ol initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}
+                className="flex  w-full gap-8  self-center lg:grid-cols-4 ">
                 <li className="text-xl font-bold  lg:text-3xl text-center text-slate-50 font-semi-bold flex flex-col">{props.range} <span className="text-sm inline-block">Autonomie
                 </span> <span className="text-sm hidden lg:inline text-one ">  (estimation de l'EPA)</span></li>
                 <li className="text-xl font-bold lg:text-3xl text-center text-slate-50 font-semi-bold flex flex-col">2.1s <span className="text-sm lg:inline">de 0 à 100km/h*</span></li>
                 <li className="   text-xl font-bold lg:text-3xl text-center text-slate-50 font-semi-bold md:flex flex-col">{props.topSpeed}<span className="text-sm  lg:inline-block">Vitesse maximal</span></li>
                 <li className=" hidden text-xl font-bold lg:flex lg:text-3xl text-center text-slate-50 font-semi-boldmd:flex flex-col md:flex md:text-xl ">1020 np <span className="text-sm hidden md:inline-block">puissance de crête</span></li>
-            </ol>
+            </motion.ol>
             <button className="text-lg px-16  m-2
 
             shadow-sm  text-slate-150  
@@ -17,6 +20,6 @@ export default function BenchComponent(props) {
             rounded-sm  bg-transparent 
             border-slate-100 border-4 py-2 text-slate-50">Commander</button>
 
-        </div>
+        </motion.div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,18 @@ import Navbar from '../components/Navbar';
 function MyApp({ Component, pageProps }) {
   return (
 
-    <main className='relative w-screen h-screen '>
-      <div className='z-20 fixed'>
-        <Navbar className="fixed" />
-      </div>
-      <Component {...pageProps} />
+    <main className=' w-screen h-screen absolute '>
+
+      <section>
+
+        <div className='bg-dark w-screen h-screen relative'>
+          <div className='absolute z-40'>
+            <Navbar />
+          </div>
+          <Component {...pageProps} />
+        </div>
+
+      </section>
     </main>)
 }
 
