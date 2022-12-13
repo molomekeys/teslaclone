61e73fd2e18dc1872f3ca542720f69a19c9187e3
Ouzar Merouane <ouzar.merouane@gmail.com>
2022-12-13 00:29:41 -0500

    fixed bug on iphone for the model S bench

--- components/BenchMark.js
+++ components/BenchMark.js
@@ -7,10 +7,10 @@ export default function BenchComponent(props) {
 
             <motion.ol initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }}
                 className="flex  w-full gap-8  self-center lg:grid-cols-4 ">
-                <li className="text-xl font-bold  lg:text-3xl text-center text-slate-50 font-semi-bold flex flex-col">{props.range} <span className="text-sm inline-block">Autonomie
+                <li className="text-xl font-bold  lg:text-3xl text-center text-slate-50 font-semi-bold flex flex-col">{props.range} 637 km <span className="text-sm inline-block">Autonomie
                 </span> <span className="text-sm hidden lg:inline text-one ">  (estimation de l'EPA)</span></li>
                 <li className="text-xl font-bold lg:text-3xl text-center text-slate-50 font-semi-bold flex flex-col">2.1s <span className="text-sm lg:inline">de 0 à 100km/h*</span></li>
-                <li className="   text-xl font-bold lg:text-3xl text-center text-slate-50 font-semi-bold md:flex flex-col">{props.topSpeed}<span className="text-sm  lg:inline-block">Vitesse maximal</span></li>
+                <li className="   text-xl font-bold lg:text-3xl text-center text-slate-50 font-semi-bold md:flex flex-col">{props.topSpeed} 2.1 <span className="text-sm  lg:inline-block">Vitesse maximal</span></li>
                 <li className=" hidden text-xl font-bold lg:flex lg:text-3xl text-center text-slate-50 font-semi-boldmd:flex flex-col md:flex md:text-xl ">1020 np <span className="text-sm hidden md:inline-block">puissance de crête</span></li>
             </motion.ol>
             <button className="text-lg px-16  m-2
